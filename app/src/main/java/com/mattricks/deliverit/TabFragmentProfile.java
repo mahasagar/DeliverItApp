5d13953b6872348fc5c627a10983cb3825207a82
mahasagar <chaudharisagard@gmail.com>
2017-01-14 08:48:45 +0530

    feat : Location Service Added

--- app/src/main/java/com/mattricks/deliverit/TabFragmentProfile.java
+++ app/src/main/java/com/mattricks/deliverit/TabFragmentProfile.java
@@ -1,15 +1,27 @@
 package com.mattricks.deliverit;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.mattricks.deliverit.utilities.SharedPreference;
 import com.roughike.bottombar.BottomBar;
 
@@ -19,12 +31,21 @@ import butterknife.ButterKnife;
 /**
  * Created by mahasagar on 12/11/16.
  */
-public class TabFragmentProfile extends Fragment {
+public class TabFragmentProfile extends Fragment implements LocationListener {
 
     private static final String STARTING_TEXT = "Four Buttons Bottom Navigation";
+    public final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
     private ViewPager mViewPager;
     BottomBar bottomBar ;
     SharedPreference sharedPreference;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private LocationRequest mLocationRequest;
+    private TextView latitude, longitude;
+
+    private double fusedLatitude = 0.0;
+    private  double fusedLongitude = 0.0;
 
     @Bind(R.id.btn_startExplore)
     Button btn_user_to_client;
@@ -46,6 +67,12 @@ public class TabFragmentProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.tabfragmentprofile, container, false);
+        initializeViews(rootView);
+
+        if (checkPlayServices()) {
+            startFusedLocation();
+            registerRequestUpdate(this);
+        }
 
         btn_user_to_client = (Button)rootView.findViewById(R.id.btn_user_to_client);
         btn_user_to_client.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +90,171 @@ public class TabFragmentProfile extends Fragment {
 
             }
         });
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
 
         return rootView;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+    private void initializeViews(View rootView) {
+        latitude = (TextView) rootView.findViewById(R.id.textview_latitude);
+        longitude = (TextView) rootView.findViewById(R.id.textview_longitude);
+    }
+
+    @Override
+    public void onStop() {
+        stopFusedLocation();
+        super.onStop();
+    }
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(getContext());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                Toast.makeText(getContext().getApplicationContext(),
+                        "This device is supported. Please download google play services", Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                Toast.makeText(getContext().getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                getActivity().finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void stopFusedLocation() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    public void registerRequestUpdate(final LocationListener listener) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // every second
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, listener);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (!isGoogleApiClientConnected()) {
+                        mGoogleApiClient.connect();
+                    }
+                    registerRequestUpdate(listener);
+                }
+            }
+        }, 1000);
+    }
+    public boolean isGoogleApiClientConnected() {
+        return mGoogleApiClient != null && mGoogleApiClient.isConnected();
+    }
+
+
+
+    public void startFusedLocation() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity()).addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnectionSuspended(int cause) {
+                        }
+
+                        @Override
+                        public void onConnected(Bundle connectionHint) {
+
+                        }
+                    }).addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+
+                        }
+                    }).build();
+            mGoogleApiClient.connect();
+        } else {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            setFusedLatitude(location.getLatitude());
+            setFusedLongitude(location.getLongitude());
+
+            Toast.makeText(getActivity().getApplicationContext(), "NEW LOCATION RECEIVED", Toast.LENGTH_LONG).show();
+
+            latitude.setText(getString(R.string.latitude_string) + " " + getFusedLatitude());
+            longitude.setText(getString(R.string.longitude_string) + " " + getFusedLongitude());
+        } catch (NullPointerException e) {
+
+        }
+
+    }
+
+    public void setFusedLatitude(double lat) {
+        fusedLatitude = lat;
+    }
+
+    public void setFusedLongitude(double lon) {
+        fusedLongitude = lon;
+    }
+
+    public double getFusedLatitude() {
+        return fusedLatitude;
+    }
+
+    public double getFusedLongitude() {
+        return fusedLongitude;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPlayServices()) {
+            startFusedLocation();
+            registerRequestUpdate(this);
+        }
+    }
 }
